558d98355b2e28d479bbb2326c452d554328ccdd
kahlys <youennlaborde@live.fr>
2018-12-06 15:39:46 +0100

    update mobimel driver

--- driver/mobimel/driver.go
+++ driver/mobimel/driver.go
@@ -21,92 +21,72 @@
 package mobimel
 
 import (
-	"errors"
-	"io/ioutil"
+	"encoding/json"
+	"fmt"
 	"net"
 	"net/http"
-	"net/url"
-	"strings"
 	"time"
 
 	"github.com/kahlys/sms"
 )
 
-var apiurl = "https://extranet.mobimel.com/envoi_form.asp"
-
-var (
-	errAPISms     = errors.New("api-sms error")
-	errAPINumber  = errors.New("api-sms: bad phone number")
-	errAPIAccount = errors.New("api-sms: bad account configuration")
-	errAPIProcess = errors.New("api-sms: something went wrong during process")
-	errAPIMsg     = errors.New("api-sms: bad message format")
-	errAPIBalance = errors.New("api-sms: no more credit")
-)
+var apiurl = "https://portail.mobimel.com/api/envoyer/sms"
 
 type mobimelDriver struct {
-	login    string
-	password string
-	sender   string
+	key     string
+	sender  string
+	typesms string
 }
 
-var errBadParams = errors.New("mobimel: missing parameter (login, password, sender)")
-
-// Init sms driver
+// Init mobimel driver
 func (md *mobimelDriver) Init(params map[string]string) error {
-	if _, ok := params["login"]; !ok {
-		return errBadParams
+	if _, ok := params["key"]; !ok {
+		return fmt.Errorf("mobimel: missing parameter key")
 	}
-	if _, ok := params["password"]; !ok {
-		return errBadParams
+	if _, ok := params["typesms"]; !ok {
+		return fmt.Errorf("mobimel: missing parameter typesms")
 	}
 	if _, ok := params["sender"]; !ok {
-		return errBadParams
+		return fmt.Errorf("mobimel: missing parameter sender")
 	}
-	md.login = params["login"]
-	md.password = params["password"]
+	md.key = params["key"]
+	md.typesms = params["typesms"]
 	md.sender = params["sender"]
 	return nil
 }
 
 // Send a message
 func (md *mobimelDriver) Send(msg, num string) error {
-	form := url.Values{}
-	form.Add("login", md.login)
-	form.Add("passwd", md.password)
-	form.Add("tel", num)
-	form.Add("msg", msg)
-	form.Add("emetteur", md.sender)
-	clt := &http.Client{
+	req, err := http.NewRequest("GET", apiurl, nil)
+	if err != nil {
+		return fmt.Errorf("mobimel: unable to create api request")
+	}
+	q := req.URL.Query()
+	q.Add("key", md.key)
+	q.Add("type", md.typesms)
+	q.Add("message", msg)
+	q.Add("destinataires", num)
+	q.Add("expediteur", md.sender)
+	req.URL.RawQuery = q.Encode()
+
+	client := http.Client{
 		Timeout: time.Second * 10,
 	}
-	response, err := clt.PostForm(apiurl, form)
+	resp, err := client.Do(req)
 	if err, ok := err.(net.Error); ok && err.Timeout() {
+		// timeout are infrequent and don't necessarily means the request errored, so the function doesn't return an error.
 		return nil
 	}
 	if err != nil {
-		return errAPISms
+		return fmt.Errorf("mobimel: unable to send request: %v", err)
 	}
-	defer response.Body.Close()
-	content, _ := ioutil.ReadAll(response.Body)
-	t := strings.Split(string(content), "/")
-	if len(t) < 3 {
-		return errAPISms
+	var body struct {
+		Result int    `json:"resultat"`
+		Errors string `json:"erreurs"`
 	}
-	switch t[0] {
-	case "0":
-		break
-	case "101", "102", "103", "104", "108", "118":
-		return errAPIAccount
-	case "105", "106", "111", "112", "4":
-		return errAPIBalance
-	case "100", "107", "1", "5", "6":
-		return errAPIProcess
-	case "117", "2", "3":
-		return errAPINumber
-	case "109", "110":
-		return errAPIMsg
-	default:
-		return errAPISms
+	json.NewDecoder(resp.Body).Decode(&body)
+	if body.Result == 0 {
+		return fmt.Errorf("mobimel api error: %v (see mobimel api documentation)", body.Errors)
 	}
 	return nil
 }
--- driver/mobimel/driver_test.go
+++ driver/mobimel/driver_test.go
@@ -1,145 +1,86 @@
 package mobimel
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestInit(t *testing.T) {
-	param := map[string]string{
-		"login":    "bruce",
-		"password": "wayne",
-		"sender":   "batman",
-	}
 	sender := &mobimelDriver{}
-
+	param := map[string]string{
+		"key":     "bruce",
+		"typesms": "wayne",
+		"sender":  "batman",
+	}
+	// init ok
 	err := sender.Init(param)
 	if err != nil {
 		t.Fatalf("wanted nil, got %s.", err)
 	}
-	if sender.login != param["login"] {
-		t.Fatalf("wanted %s, got %s.", param["login"], sender.login)
+	if sender.key != param["key"] {
+		t.Fatalf("wanted %s, got %s.", param["login"], sender.key)
 	}
-	if sender.password != param["password"] {
-		t.Fatalf("wanted %s, got %s.", param["password"], sender.password)
+	if sender.typesms != param["typesms"] {
+		t.Fatalf("wanted %s, got %s.", param["password"], sender.typesms)
 	}
 	if sender.sender != param["sender"] {
 		t.Fatalf("wanted %s, got %s.", param["sender"], sender.sender)
 	}
-}
-
-func TestInitErr(t *testing.T) {
-	cases := []map[string]string{
-		{
-			"password": "wayne",
-			"sender":   "batman",
-		},
-		{
-			"login":  "bruce",
-			"sender": "batman",
-		},
-		{
-			"login":    "bruce",
-			"password": "wayne",
-		},
+	// init error missing parameter
+	delete(param, "sender")
+	err = sender.Init(param)
+	if err == nil {
+		t.Fatalf("expected an error")
+	}
+	delete(param, "typesms")
+	err = sender.Init(param)
+	if err == nil {
+		t.Fatalf("expected an error")
+	}
+	delete(param, "key")
+	err = sender.Init(param)
+	if err == nil {
+		t.Fatalf("expected an error")
 	}
 
-	for _, c := range cases {
-		sender := &mobimelDriver{}
-		if err := sender.Init(c); err != errBadParams {
-			t.Fatalf("wanted %s, got %s.", errBadParams, err)
-		}
-	}
 }
 
 func TestSend(t *testing.T) {
 	sender := &mobimelDriver{}
 
-	tests := []struct {
-		responder *httptest.Server
-		number    string
-		message   string
-		wantedErr error
+	cases := []struct {
+		responder   *httptest.Server
+		number      string
+		message     string
+		expectedErr bool
 	}{
 		{
 			responder: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				w.WriteHeader(http.StatusOK)
-				w.Write([]byte("0/+33605040302/Le message a été envoyé."))
+				json.NewEncoder(w).Encode(map[string]interface{}{"resultat": 1})
 			})),
-			number:    "+33605040302",
-			message:   "test",
-			wantedErr: nil,
+			number:      "+33605040302",
+			message:     "test",
+			expectedErr: false,
 		},
 		{
 			responder: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				w.WriteHeader(http.StatusOK)
-				w.Write([]byte("47/"))
+				json.NewEncoder(w).Encode(map[string]interface{}{"resultat": 0, "erreurs": "2"})
 			})),
-			number:    "+33605040302",
-			message:   "test",
-			wantedErr: errAPISms,
-		},
-		{
-			responder: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				w.WriteHeader(http.StatusOK)
-				w.Write([]byte("47/+33605040302/This is a new millenium."))
-			})),
-			number:    "+33605040302",
-			message:   "test",
-			wantedErr: errAPISms,
-		},
-		{
-			responder: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				w.WriteHeader(http.StatusOK)
-				w.Write([]byte("2/0102030405/Le numéro n'est pas valide."))
-			})),
-			number:    "+33605040302",
-			message:   "test",
-			wantedErr: errAPINumber,
-		},
-		{
-			responder: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				w.WriteHeader(http.StatusOK)
-				w.Write([]byte("101/+33605040302/Vous n'êtes pas autorisé à envoyer des messages."))
-			})),
-			number:    "+33605040302",
-			message:   "test",
-			wantedErr: errAPIAccount,
-		},
-		{
-			responder: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				w.WriteHeader(http.StatusOK)
-				w.Write([]byte("107/+33605040302/Service momentanément indisponible (maintenance)."))
-			})),
-			number:    "+33605040302",
-			message:   "test",
-			wantedErr: errAPIProcess,
-		},
-		{
-			responder: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				w.WriteHeader(http.StatusOK)
-				w.Write([]byte("109/+33605040302/Message absent ou vide."))
-			})),
-			number:    "+33605040302",
-			message:   "test",
-			wantedErr: errAPIMsg,
-		},
-		{
-			responder: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				w.WriteHeader(http.StatusOK)
-				w.Write([]byte("105/+33605040302/Erreur : votre solde est épuisé."))
-			})),
-			number:    "+33605040302",
-			message:   "test",
-			wantedErr: errAPIBalance,
+			number:      "+33605040302",
+			message:     "test",
+			expectedErr: true,
 		},
 	}
 
-	for _, tc := range tests {
-		apiurl = tc.responder.URL
-		if err := sender.Send(tc.number, tc.number); err != tc.wantedErr {
-			t.Fatalf("wanted %s, got %s.", tc.wantedErr, err)
+	for _, c := range cases {
+		apiurl = c.responder.URL
+		err := sender.Send(c.number, c.number)
+		if err == nil && c.expectedErr {
+			t.Errorf("expected an error")
 		}
 	}
 }
